f91d662c25586c2077613f3d33d45bc21c76745e
bennycive <bennycive@gmail.com>
2025-01-25 23:45:05 +0300

    about components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,12 +6,14 @@ import { WeatherComponent } from './landing/weather/weather.component';
 import { CropsComponent } from './landing/crops/crops.component';
 import { ContactsComponent } from './landing/contacts/contacts.component';
 import { ServicesComponent } from './landing/services/services.component';
+import { AboutComponent } from './landing/about/about.component';
 
 
 export const routes: Routes = [
   // Landing Page Routes
   { path: '', component: LandingPageComponent },
   { path: 'weather', component: WeatherComponent },
+  { path: 'about', component: AboutComponent },
   { path: 'crops', component: CropsComponent },
   { path: 'contacts', component: ContactsComponent },
   { path: 'services', component: ServicesComponent},
