6af129a5d2c4588a7b9634ce93a39044f5ffe83c
bennycive <bennycive@gmail.com>
2025-01-26 02:36:26 +0300

    mobile menu show

--- src/app/landing/header/header.component.ts
+++ src/app/landing/header/header.component.ts
@@ -1,11 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
+
+
+  isMenuVisible: boolean = false;
+  isMenuOpen: any;
+
+  toggleMenu() {
+    this.isMenuVisible = !this.isMenuVisible;
+  }
+
 
 }
