02fb7fd6d7ee010f4833642141ee80c3755992ae
bennycive <bennycive@gmail.com>
2025-04-24 08:27:09 +0300

    preloader fix

--- src/app/preloader/preloader.component.ts
+++ src/app/preloader/preloader.component.ts
@@ -1,23 +1,29 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-
+import { Router, NavigationStart, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-preloader',
+  standalone: true,
   imports: [CommonModule],
   templateUrl: './preloader.component.html',
-  styleUrl: './preloader.component.css'
+  styleUrls: ['./preloader.component.css']
 })
-
-
 export class PreloaderComponent implements OnInit {
   isLoading: boolean = true;
 
-  constructor() { }
+  constructor(private router: Router) {}
 
   ngOnInit(): void {
-    window.onload = () => {
-      this.isLoading = false;
-    };
+    this.router.events.subscribe(event => {
+      if (event instanceof NavigationStart) {
+        this.isLoading = true;
+      } else if (event instanceof NavigationEnd) {
+        // Small delay to visualize the loader
+        setTimeout(() => {
+          this.isLoading = false;
+        }, 500);
+      }
+    });
   }
 }
